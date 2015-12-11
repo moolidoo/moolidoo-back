b8f4ace37ee5739e54bbe6aa8985c97522f0f8b4
Davide Bianchi <davide.bianchi@mondora.com>
2015-12-11 14:10:17 +0100

    Add control for not add tips to yourself

--- server/users/methods.js
+++ server/users/methods.js
@@ -1,5 +1,10 @@
 Meteor.methods({
-    addTips: function (numTips, targetUserId) {
+    addTips: function (targetUserId) {
+        const numTips = 1;
+        const userId = Meteor.userId();
+        if (userId == targetUserId) {
+            throw new Meteor.Error("Cannot add tips to yourself");
+        }
         Meteor.users.update({_id: targetUserId}, {
             $inc: {
                 tips: numTips
